6b97e655f171f1425af6fbdbf938ae0e81c6501d
Okada-Ren-Fukuyama <gc23007@anabuki-net.ne.jp>
2024-03-29 00:38:57 +0900

    37

--- 3DLv1_vs2019_00/GameProgramming/src/CApplication.h
+++ 3DLv1_vs2019_00/GameProgramming/src/CApplication.h
@@ -24,7 +24,7 @@
 class CApplication
 {
 private:
-	~CApplication();
+	
 	static CUi* spUi;	//UIクラスのポインタ
 	//モデルからコライダを生成
 	CColliderMesh mColliderMesh;
@@ -63,6 +63,7 @@ private:
 	//CCharacterのポインタの可変長配列
 //	std::vector<CCharacter*> mCharacters;
 public:
+	~CApplication();
 	static CUi* Ui();	//UIクラスのインスタンスを取得
 	//モデルビュー行列の取得
 	static const CMatrix& ModelViewInverse();
